8756bcf577ba7aeef7925e06c20cc2e0e219ff9e
Diego Alonso Molina <diegoalonso@tuniversformacion.es>
2023-10-05 12:04:11 +0200

    Ejercicio de ejemplo de botón de hamburguesa

---
+++ scripts/script.js
@@ -0,0 +1,31 @@
+/**
+ * Autor: Diego Alonso Molina (Full Stack Developer)
+ * GitHub: 
+ */
+
+// Seleccionamos los dos elementos que serán clickables
+
+const toggleButton = document.getElementById("button-menu");
+const navWrapper = document.getElementById("nav");
+
+/* 
+  Cada ves que se haga click en el botón 
+  agrega y quita las clases necesarias 
+  para que el menú se muestre.
+*/
+toggleButton.addEventListener("click", () => {
+  toggleButton.classList.toggle("close");
+  navWrapper.classList.toggle("show");
+});
+
+/* 
+  Cuándo se haga click fuera del contenedor de enlaces 
+  el menú debe esconderse.
+*/
+
+navWrapper.addEventListener("click", e => {
+  if (e.target.id === "nav") {
+    navWrapper.classList.remove("show");
+    toggleButton.classList.remove("close");
+  }
+});
